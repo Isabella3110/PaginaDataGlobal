e6760907a1d335209bec5f0c80027aec729aa913
isabella cañas <isacanasduc@gmail.com>
2025-08-05 19:36:48 -0500

    organizamos las carpetas en pages y creamos nuevos componentes

---
+++ app/components/BannerPrincipal.vue
@@ -0,0 +1,82 @@
+<template>
+    <section
+        class="bg-[url('https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/coast-house-view.jpg')] bg-no-repeat bg-cover bg-center bg-gray-700 bg-blend-multiply ">
+        <div class="relative py-8 px-4 mx-auto max-w-screen-xl text-white lg:py-16 z-1">
+            <div class="mb-6 max-w-screen-lg lg:mb-0">
+                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl">Impulsamos
+                    tu Potencial Digital</h1>
+                <p class="mb-6 font-light text-gray-400 lg:mb-8 md:text-lg lg:text-xl">Somos tu socio estratégico en
+                    desarrollo de software, especializados en crear páginas web, aplicaciones y soluciones digitales a
+                    medida que impulsan tu negocio hacia el éxito</p>
+                <a href="#"
+                    class="inline-flex items-center py-3 px-5 font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-900 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                    Conoce Nuestros Servicios →
+                    <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </a>
+            </div>
+            <div class="grid gap-8 pt-8 lg:pt-12 mt-8 lg:mt-12 border-t border-gray-600 sm:grid-cols-2 lg:grid-cols-4">
+                <div>
+                    <h2 class="mb-1 text-lg font-bold">Desarrollo Web Personalizado</h2>
+                    <p class="mb-1 text-sm text-gray-400">Desde sitios informativos hasta e-commerce robustos, creamos
+                        páginas web únicas y escalables para tu negocio.</p>
+                    <a href="#" class="inline-flex items-center text-sm font-semibold text-primary-500 hover:underline">
+                        Explora el Desarrollo Web
+                        <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div>
+                    <h2 class="mb-1 text-lg font-bold">Resultados Comprobados</h2>
+                    <p class="mb-1 text-sm text-gray-400">Nuestro portafolio de proyectos exitosos habla por sí mismo.
+                        Clientes satisfechos y objetivos alcanzados.</p>
+                    <a href="#" class="inline-flex items-center text-sm font-semibold text-primary-500 hover:underline">
+                        Ver Casos de Éxito
+                        <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div>
+                    <h2 class="mb-1 text-lg font-bold">Rendimiento y Optimización Garantizada</h2>
+                    <p class="mb-1 text-sm text-gray-400">Construimos software que no solo funciona, sino que lo hace de
+                        manera rápida y eficiente, sin comprometer el rendimiento </p>
+                    <a href="#" class="inline-flex items-center text-sm font-semibold text-primary-500 hover:underline">
+                        Contactanos
+                        <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
+                </div>
+                <div>
+                    <h2 class="mb-1 text-lg font-bold">Equipos Multidisciplinarios Expertos</h2>
+                    <p class="mb-1 text-sm text-gray-400">Contamos con talentos especializados en desarrollo, diseño y
+                        estrategia para tu proyecto integral</p>
+                    <a href="#" class="inline-flex items-center text-sm font-semibold text-primary-500 hover:underline">
+                        Conoce al equipo
+                        <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <BannerPrincipal />
+  <div class="p-8 text-center">
+  </div>
+</template>
